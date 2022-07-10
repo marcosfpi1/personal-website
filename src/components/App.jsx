25cbe6468cee92414e7071e86a2beec6208a75bf
Marcos Pi Marrero <mpimarre@Marcoss-MacBook-Air.local>
2022-07-10 15:24:16 -0400

    Added image support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,22 @@
 import  "../styles/index.scss";
 import Recipes from "./Recipes";
+import Supra from "../images/supra.png";
+import LesPaul from "../images/lespaul.svg";
 
 const App = () => {
     return (
         <>
-            <section className="test">
+            <section className="hero">
 
             </section>
             <main>
-                <section className="test">
+                <section>
                     <h1>Hello!</h1>
                 </section>
+                <img src={Supra} alt="Supra" width="250" />
+                <img src={LesPaul} alt="LesPaul" width="250" />
+                <Recipes />
             </main>
-
-            <Recipes />
         </>
     );
 };
