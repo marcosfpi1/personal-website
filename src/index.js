69451f20cef1943c48b0be39dc67db572a0802fe
Marcos Pi Marrero <mpimarre@Marcoss-MacBook-Air.local>
2022-07-10 14:03:58 -0400

    Added css stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
+import  "./styles/index.scss";
+
 const greetings = {
     hello: 1,
     bye: 2,
     hey: 3
-}
+};
 
 const words = {
     ...greetings,
     food: 4,
     work: 5
-}
+};
 
 console.log(greetings);
 console.log(words);
